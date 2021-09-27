126fd5a176941d60068287e598ca671d69fe9e6f
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2021-09-27 19:36:15 +0200

    Print lein profiles

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [com.taoensso/timbre "5.1.2"]
                  [digest "1.4.10"]
                  [eftest "0.5.9"]  ; testing library
-                 [expound "0.8.9"] ; error messages for clojure spec
+                 [expound "0.8.10"] ; error messages for clojure spec
                  [io.joshmiller/exif-processor "0.2.0"]]
   :main ^:skip-aot clj-photo-org.core
   :target-path "target/%s"
